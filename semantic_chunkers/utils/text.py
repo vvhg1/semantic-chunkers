1349dffe8c0546aa6d9ff13383781d641c8b0c0a
vvhg1 <victor.hobe.gelting@gmail.com>
2024-06-04 11:56:08 +0200

    changed tokenizer to llama.cpp

--- semantic_chunkers/utils/text.py
+++ semantic_chunkers/utils/text.py
@@ -1,7 +1,12 @@
-import tiktoken
-
-
 def tiktoken_length(text: str) -> int:
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    tokens = tokenizer.encode(text, disallowed_special=())
-    return len(tokens)
+    # instead we use the llama.cpp tokenizer
+    tokenizer_command = f'~/repos/llama.cpp/tokenize ~/repos/llama.cpp/models/dolphin-2.2.1-mistral-7b.Q5_K_M.gguf "{text}"'
+    import subprocess
+
+    process = subprocess.Popen(
+        tokenizer_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate()
+    count = stdout.decode().count("\n")
+
+    return count
