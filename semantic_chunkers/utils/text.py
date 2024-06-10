e483b3e030dce4700b428efe179f228ff2f7f156
vvhg1 <victor.hobe.gelting@gmail.com>
2024-06-10 20:28:49 +0200

    now working with current llama.cpp

--- semantic_chunkers/utils/text.py
+++ semantic_chunkers/utils/text.py
@@ -1,12 +1,12 @@
 def tiktoken_length(text: str) -> int:
     # instead we use the llama.cpp tokenizer
-    tokenizer_command = f'~/repos/llama.cpp/tokenize ~/repos/llama.cpp/models/dolphin-2.2.1-mistral-7b.Q5_K_M.gguf "{text}"'
+    tokenizer_command = f'~/repos/llama.cpp/tokenize -m ~/repos/llama.cpp/models/dolphin-2.2.1-mistral-7b.Q5_K_M.gguf --prompt "{text}"'  # changed so it works with the current llama.cpp
     import subprocess
 
     process = subprocess.Popen(
         tokenizer_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    stdout, stderr = process.communicate()
-    count = stdout.decode().count("\n")
+    lines = process.stdout.readlines()
+    count = len(lines)
 
     return count
